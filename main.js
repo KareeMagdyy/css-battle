5b027c3029d43b3ecf3cdcb2db395712e4e7d548
KareeMagdyy <karim67899@gmail.com>
2021-10-08 16:42:58 +0200

    battle# 1~3

---
+++ main.js
@@ -0,0 +1,16 @@
+const battlesCount = 3;
+const list = document.querySelector(".battles");
+const baseURL = "https://img.icons8.com/ios-filled/50/000000/";
+
+for (let i = 1; i <= battlesCount; i++) {
+  const item = document.createElement("li");
+  const newLink = document.createElement("a");
+  newLink.href = `${i}/index.html`;
+  newLink.target = `__blank`;
+  const newImg = document.createElement("img");
+  newImg.src = `${baseURL}${i}.png`;
+
+  newLink.appendChild(newImg);
+  item.appendChild(newLink);
+  list.appendChild(item);
+}
